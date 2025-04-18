4bdf39c613d736a452a07d41bc34c2300c0324cd
AndriyShmidt <andrii.shmidt@snagasolutions.com>
2025-04-19 01:01:05 +0300

    add useEffect to fetch and dispatch companies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { fetchCompanies } from './api/api';
+import { Company } from './react-app-env';
+import { setCompanies } from './store';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getDataFromServer = async () => {
+      try {
+        const response = await fetchCompanies();
+
+        const data: Company[] = await response;
+        dispatch(setCompanies(data));
+      } catch (error) {
+        alert('Error try again');
+      }
+    };
+
+    getDataFromServer();
+  }, [dispatch]);
+  
   return (
     <div className="App">
     </div>
